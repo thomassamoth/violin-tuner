586a96b9f33f112b4556377eefbaae678068d26a
thomassamoth <thomassamoth.dev@gmail.com>
2021-09-27 17:47:05 +0200

    feat(): keep checking the chosen note while it's wrong * stop leaving the program when the note is wrong

--- final.py
+++ final.py
@@ -9,25 +9,34 @@ from numpy.fft import fft
 import os
 import time
 
-chosen_note = input('\nChoose a note to check (English naming convention) : ')
 
-note_frequency_dict = { 
+note_frequency_dict = {
     "G": 196,
     "D": 294,
     "A": 440,
     "E": 660
 }
 
-if chosen_note not in note_frequency_dict:
-    print("Chosen note \""+chosen_note+"\" invalid")
-    exit(1)
 
+def ask_note():
+
+    chosen_note = input(
+        '\nChoose a note to check (English naming convention) : ')
+
+    if chosen_note not in note_frequency_dict:
+        print("Chosen note \""+chosen_note+"\" invalid")
+        chosen_note = ask_note()
+        # exit(1)
+    return chosen_note
+
+
+chosen_note = ask_note()
 target_frequency = note_frequency_dict[chosen_note]
 
 print('Target Frequency = ', target_frequency, 'Hz\n')
 
 # Pause in the program for the user to prepare the recording
-PAUSE = 3
+PAUSE = 3  # seconds
 print('Pause of ', PAUSE, 'seconds underway - Prepare for recording')
 time.sleep(PAUSE)
 
@@ -35,15 +44,12 @@ time.sleep(PAUSE/10)
 # -----------------------------------
 '''RECORDING'''
 
-chosen_note = str(chosen_note)
-target_frequency = str(target_frequency)
-
 FORMAT = pyaudio.paInt16
-CHANNELS = 1 # record in mono 
+CHANNELS = 1  # record in mono
 RATE = 44100
 CHUNK = 1024
 RECORD_SECONDS = 2    # seconds
-WAVE_OUTPUT_FILENAME = target_frequency + ".wav"
+WAVE_OUTPUT_FILENAME = str(target_frequency) + ".wav"
 
 audio = pyaudio.PyAudio()
 
@@ -60,7 +66,7 @@ for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
 
-print("\n Recording completed :-D ")
+print("\n Recording completed")
 
 
 # Fin enregistrement
@@ -78,15 +84,15 @@ waveFile.close()
 ''' Fast Fourier Transform '''
 
 # get the file we recorded
-FILE = os.path.join(target_frequency+".wav")
+FILE = os.path.join(str(target_frequency)+".wav")
 
 # Play the sound file
-RATE, DATA = sciwave.read(FILE) # get the sample rate
+RATE, DATA = sciwave.read(FILE)  # get the sample rate
 TAB = np.array(DATA)
 
-# To get all the values from the array (debug)
-#np.set_printoptions(threshold=sys.maxsize)
-#print('tab=',TAB)
+# To get all the values from the array (--debug--)
+# np.set_printoptions(threshold=sys.maxsize)
+# print('tab=',TAB)
 
 n = DATA.size
 DUREE = 1.0*n/RATE
@@ -115,11 +121,12 @@ def tracerFFT(DATA, RATE, debut, DUREE):
 
     freq = np.zeros(spectre_size)
     frequence_jouee_interne = 0
-    # go through all the frequencies and 
+
     for k in range(spectre_size):
         freq[k] = 1.0/spectre_size*RATE*k
         if spectre[k] == 1 and freq[k] < 1500:
             frequence_jouee_interne = freq[k]
+
     # frequency, background color, spectre, line colour
     vlines(freq, 0, spectre, 'r')
     xlabel('Frequency (Hz)')
@@ -127,13 +134,28 @@ def tracerFFT(DATA, RATE, debut, DUREE):
     title('Fourier Transform')
     axis([0, 0.5*RATE, 0, 1])
     grid()
-    return [frequence_jouee_interne]
+    return frequence_jouee_interne
 
 
 # Display the FFT
-figure(figsize=(12, 4))  # sets the window size
+figure(figsize=(12, 4))  # sets the window sizeA
 PlayedFrequency = tracerFFT(DATA, RATE, 0.1, 0.5)  # DATA,RATE,debut,DUREE
 axis([0, 1000, 0, 1])  # axes xmin,xmax,ymin,ymax
-PlayedFrequency = str(PlayedFrequency)
-print("\n" + 'Played frequency ', chosen_note, "=", (PlayedFrequency), "Hz")
+print("\033[96m\n" + 'Played frequency ', chosen_note, "=",
+      str(PlayedFrequency), "Hz\n\x1B[37m")  # print in cyan
+
 show()  # display the graph
+
+
+def recording_error(PlayedFrequency): # user get an error message if the played frequency is 0
+
+    if PlayedFrequency == '0':
+        print("\n======  ERROR =====")
+        print("\x1B[37mThere's been an error while recording \033[91m")
+        print("Probable cause: The microphone is too far away from the audio source")
+        print("\x1B[37mPlease try again !\n")
+
+    
+
+recording_error(PlayedFrequency)
+
